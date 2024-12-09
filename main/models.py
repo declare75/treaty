13f8ce3aae6cbe86e302501a32dff43351aa2118
dinmix <egmaxim343@gmail.com>
2024-12-09 22:43:30 +0300

    все крутое тут

--- main/models.py
+++ main/models.py
@@ -14,14 +14,20 @@ class CustomUserManager(BaseUserManager):
         email = self.normalize_email(email)
         # Убедитесь, что 'username' не передается в `self.model`
         if 'username' in extra_fields:
-            del extra_fields['username']  # Удаляем 'username' если оно передано
+            del extra_fields['username']  # Удаляем 'username', если оно передано
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError("Superuser must have is_staff=True.")
+        if not extra_fields.get('is_superuser'):
+            raise ValueError("Superuser must have is_superuser=True.")
 
         return self.create_user(email, password, **extra_fields)
 
