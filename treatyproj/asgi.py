71b85c60d23a3c2564ef181fb58ad45c7b98bace
dinmix <egmaxim343@gmail.com>
2025-05-07 18:44:13 +0300

    123

--- treatyproj/asgi.py
+++ treatyproj/asgi.py
@@ -1,18 +1,15 @@
 import os
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing  # Убедитесь, что путь к chat.routing правильный
+from channels.auth import AuthMiddlewareStack
+import videocall.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'treatyproj.settings')
-
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                chat.routing.websocket_urlpatterns  # Используется правильный путь
-            )
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            videocall.routing.websocket_urlpatterns
+        )
+    )
+})
